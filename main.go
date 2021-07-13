64a2691c4de972b4dc67b7564467cd3b773b9390
Rudge Renan Ferreira <rudge.ferreira@c6bank.com>
2021-07-13 20:49:00 -0300

    Initial committ

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/gofiber/fiber"
+)
+
+type Token struct {
+	AccessToken string `json:"access_token"`
+	TokenType   string `json:"token_type"`
+}
+
+func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.JSON(&Token{"access", "token"})
+	})
+
+	app.Listen(":" + port)
+}
